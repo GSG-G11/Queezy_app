b3e558afe1706f58bc670efc82addb93530447c9
SalmaIssa96 <salmaissa196@gmail.com>
2022-01-20 12:10:15 +0200

    done with adding the questions in the quizbank file relates#11

--- Assets/quizbank.js
+++ Assets/quizbank.js
@@ -1,7 +1,119 @@
 export const questions = [
   {
-    question: "test",
-    answers: ["answer1", "answer2", "answer3", "answer4"],
-    conrrect: "answer1",
+    question: "_____ her name Eliza? No, _____ .",
+    answers: ["is / from", " isn’t / from", "isn’t / in ", "aren’t / in"],
+    answer: "Is / it isn't",
+  },
+  {
+    question: "I ... tennis every Sunday morning.",
+    answers: ["playing", "play", " am playing", " am play"],
+    answer: "play",
+  },
+  {
+    question:
+      "Don't make so much noise. Noriko ...... to study for her ESL test!",
+    answers: ["try", "tries", "tried", "	is trying"],
+    answer: "	is trying",
+  },
+  {
+    question: "Jun-Sik .... his teeth before breakfast every morning.",
+    answers: [" will cleaned", "	is cleaning", "cleans", "clean"],
+    answer: "cleans",
+  },
+  {
+    question: "Sorry, she can't come to the phone. She ..... a bath!",
+    answers: ["is having", "having", "have", "has"],
+    answer: "is having",
+  },
+  {
+    question: "..... many times every winter in Frankfurt.",
+    answers: [" It snows", "	It snowed", "	It is snowing", "	It is snow"],
+    answer: "It snows",
+  },
+  {
+    question: "How many students in your class ..... from Korea?",
+    answers: ["comes", "come", "came", "are coming"],
+    answer: "come",
+  },
+  {
+    question: ` Weather report: "It's seven o'clock in Frankfurt and ..... ."`,
+    answers: [" there is snow", " it`s snowing", "it snows", "it snowed"],
+    answer: "it`s snowing",
+  },
+  {
+    question: "Babies ..... when they are hungry.",
+    answers: ["cry", "cries", "cried", "  are crying"],
+    answer: "cry",
+  },
+  {
+    question: ` What ..... in the evenings?"
+    Mary: "Usually I watch TV or read a book.`,
+    answers: ["you doing", "you do", "do you do", "	are you doing"],
+    answer: "do you do",
+  },
+  {
+    question: ` What ..... ?"
+    Mary: "I'm trying to fix my calculator..`,
+    answers: ["you doing", "you do", "do you do", "are you doing"],
+    answer: "are you doing",
+  },
+  {
+    question:
+      "Jane ..... her blue jeans today, but usually she wears a skirt or a dress.",
+    answers: ["wears", "wearing", "wear", " is wearing"],
+    answer: "is wearing",
+  },
+  {
+    question:
+      "	I think I ..... a new calculator. This one does not work properly any more.",
+    answers: ["needs", "needed", "need", "am needing"],
+    answer: "need",
+  },
+  {
+    question: "Sorry, you can't borrow my pencil. I ..... it myself.	",
+    answers: ["was using", "using", " use", " am using"],
+    answer: "am using",
+  },
+  {
+    question: `At a school dance:
+    Jane: " ..... yourself?"
+    Mary: "Yes, I'm having a great time!"`,
+    answers: ["You enjoying", "Enjoy you", "Do you enjoy", " Are you enjoying"],
+    answer: "Are you enjoying",
+  },
+  {
+    question: "What time .....",
+    answers: [
+      "the train leaves?",
+      "leaves the train?",
+      "  is the train leaving?",
+      "does the train leave?",
+    ],
+    answer: "does the train leave?",
+  },
+  {
+    question: "What time .....",
+    answers: [
+      "the train leaves?",
+      "leaves the train?",
+      "  is the train leaving?",
+      "does the train leave?",
+    ],
+    answer: "does the train leave?",
+  },
+  {
+    question: "You can keep my iPod if you like. I ..... it any more.",
+    answers: [" don`t use", " doesn`t use", "   didn`t use", "  am not using"],
+    answer: "don`t use",
+  },
+  {
+    question: "I ..... for my pen. Have you seen it?",
+    answers: ["will look", "    looking", "look", "am looking"],
+    answer: "am looking",
+  },
+  {
+    question: "The phone ...... Can you answer it, please?",
+    answers: ["rings", "ring", "rang", " is ringing"],
+    answer: "is ringing",
   },
 ];
